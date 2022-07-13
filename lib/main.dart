829472617b308fa101f22b54b3cf8a1384c3061f
ハリム　ムハンマド アブドゥル <xadi.halim.md.abdul@sapporo.ad.icom.local>
2022-07-13 15:11:56 +0900

    Layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MyApp()
-  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -11,17 +11,84 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return 
-    MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Container(
-            height: 100.0,
-            width: 100.0,
-            margin: EdgeInsets.symmetric(vertical: 50.0),
-            color: Colors.white,
-            child: Text("Thsi s"),
+          child: Column(
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage("assets/images/me.png"),
+              ),
+              Text(
+                "Halim",
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                "Flutter Dev",
+                style: TextStyle(
+                  fontFamily: 'sanspro',
+                  fontSize: 15.0,
+                  color: Colors.black54,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 3.0,
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(vertical: 10.0,horizontal:25.0),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.phone,
+                      color: Colors.black54,
+                    ),
+                    Text(
+                      "+8107021656683",
+                      style: TextStyle(
+                        fontFamily: 'sanspro',
+                        fontSize: 15.0,
+                        color: Colors.black54,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 3.0,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(vertical: 10.0,horizontal:25.0),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.email,
+                      color: Colors.black54,
+                    ),
+                    SizedBox(width: 10.0,),
+                    Text(
+                      "reiazbubt@gmail.com",
+                      style: TextStyle(
+                        fontFamily: 'sanspro',
+                        fontSize: 15.0,
+                        color: Colors.black54,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 3.0,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
       ),
