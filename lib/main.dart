35861539f8476dde29b05d6a7a891bf97faba37e
ハリム　ムハンマド アブドゥル <xadi.halim.md.abdul@sapporo.ad.icom.local>
2022-07-13 11:08:52 +0900

    what is run app.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      backgroundColor: Colors.black87,
-      body: Container(),
-    ),
-  ),);
+  runApp(
+    MyApp()
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return 
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: Container(),
+      ),
+    );
+  }
 }
